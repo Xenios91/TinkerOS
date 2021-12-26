7a6f14a9f3adfe7cba4051ea49e1aef15f2f4d1f
Corey <xenios91@protonmail.com>
2021-12-25 23:46:50 -0600

    copied from tutorial, had weird bug

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -3,18 +3,18 @@ global start
 section .text
 bits 32
 
-setup_page_table:
+start:
     ; Point the first entry of the level 4 page table to the first entry in the
     ; p3 table
     mov eax, p3_table
-    or eax, 0b11
-    mov dword [p4_table], eax
+    or eax, 0b11 ; 
+    mov dword [p4_table + 0], eax
 
     ; Point the first entry of the level 3 page table to the first entry in the
     ; p2 table
     mov eax, p2_table
     or eax, 0b11
-    mov dword [p3_table], eax
+    mov dword [p3_table + 0], eax
 
     ; point each page table level two entry to a page
     mov ecx, 0         ; counter variable
@@ -28,9 +28,10 @@ setup_page_table:
     cmp ecx, 512
     jne .map_p2_table
 
-    ret
+    ; move page table address to cr3
+    mov eax, p4_table
+    mov cr3, eax
 
-enable_physical_address_extension:
     ; enable PAE
     mov eax, cr4
     or eax, 1 << 5
@@ -42,38 +43,25 @@ enable_physical_address_extension:
     or eax, 1 << 8
     wrmsr
 
-    ret
-
-enable_paging:
     ; enable paging
     mov eax, cr0
     or eax, 1 << 31
     or eax, 1 << 16
     mov cr0, eax
 
-    ret
-
-load_message:
-    mov word [0xb8000], 0x0E54 ; T
-    mov word [0xb8002], 0x0E49 ; I
-    mov word [0xb8004], 0x0E4E ; N
-    mov word [0xb8006], 0x0E4B ; K
-    mov word [0xb8008], 0x0E45 ; E
-    mov word [0xb800a], 0x0E52 ; R
-    mov word [0xb800c], 0x0E20 ;
-    mov word [0xb800e], 0x0E4F ; O
-    mov word [0xb8010], 0x0E53 ; S
-
-    ret
-
-start:
-    call load_message
-    call setup_page_table
-    call enable_physical_address_extension
-    call enable_paging
+    ; load the GDT
     lgdt [gdt64.pointer]
 
-    hlt ; halt
+    ; update selectors
+    mov ax, gdt64.data
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+
+    ; jump to long mode!
+    jmp gdt64.code:long_mode_start
+
+    hlt
 
 section .bss
 
@@ -96,3 +84,22 @@ gdt64:
 .pointer:
     dw .pointer - gdt64 - 1
     dq gdt64
+
+section .text
+bits 64
+long_mode_start:
+    call load_message
+    hlt
+
+load_message:
+    mov word [0xb8000], 0x0E54 ; T
+    mov word [0xb8002], 0x0E49 ; I
+    mov word [0xb8004], 0x0E4E ; N
+    mov word [0xb8006], 0x0E4B ; K
+    mov word [0xb8008], 0x0E45 ; E
+    mov word [0xb800a], 0x0E52 ; R
+    mov word [0xb800c], 0x0E20 ;
+    mov word [0xb800e], 0x0E4F ; O
+    mov word [0xb8010], 0x0E53 ; S
+
+    ret
